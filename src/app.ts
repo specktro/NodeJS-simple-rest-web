463e36e5698ff5c1863b195fb526757a43290198
@specktro <specktro@nonull.mx>
2025-03-22 21:49:01 -0600

    feat: Serve files in a bad way, analyzing the request and write the file in the response

--- src/app.ts
+++ src/app.ts
@@ -6,10 +6,17 @@ const server = http.createServer((request, response) => {
         const htmlFile = fs.readFileSync('./public/index.html', 'utf-8')
         response.writeHead(200, {'Content-Type': 'text/html'})
         response.end(htmlFile)
-    } else {
-        response.writeHead(404, {'Content-Type': 'text/html'})
-        response.end()
+        return
     }
+
+    if (request.url?.endsWith('.css')) {
+        response.writeHead(200, {'Content-Type': 'text/css'})
+    } else if (request.url?.endsWith('.js')) {
+        response.writeHead(200, {'Content-Type': 'application/javascript'})
+    }
+
+    const responseFile = fs.readFileSync(`./public${request.url}`, 'utf-8')
+    response.end(responseFile)
 })
 
 server.listen(8080, () => {
