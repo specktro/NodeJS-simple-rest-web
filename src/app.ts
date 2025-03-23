13d8722f8375d041324ced0d8eb85d81b323b6b5
@specktro <specktro@nonull.mx>
2025-03-23 16:32:18 -0600

    feat: Add environment variables to server

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 import {Server} from './presentation/server'
+import {envs} from "./config/envs";
 
 (async () => {
     await main()
 })()
 
 async function main() {
-    const server = new Server()
+    const server = new Server({
+        port: envs.PORT,
+        publicPath: envs.PUBLIC_PATH,
+    })
     await server.start()
 }
