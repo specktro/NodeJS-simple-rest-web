e228253ec060cb7e08dd7c2359b356e264071be0
@specktro <specktro@nonull.mx>
2025-03-30 08:04:42 -0600

    feat: Implement Read from CRUD for ToDo's

--- src/presentation/todos/controller.ts
+++ src/presentation/todos/controller.ts
@@ -1,13 +1,33 @@
 import {Request, Response} from 'express'
 
+const toDos = [
+    {id: 1, text: 'Buy milk', createdAt: new Date()},
+    {id: 2, text: 'Buy bread', createdAt: null},
+    {id: 3, text: 'Buy butter', createdAt: new Date()}
+]
+
 export class ToDosController {
     constructor() {}
 
     public getToDos = (req: Request, res: Response) => {
-        res.json([
-            {id: 1, text: 'Buy milk', createdAt: new Date()},
-            {id: 2, text: 'Buy bread', createdAt: null},
-            {id: 3, text: 'Buy butter', createdAt: new Date()}
-        ])
+        res.json(toDos)
+    }
+
+    public getToDoById = (req: Request, res: Response) => {
+        const id = +req.params.id
+
+        if (isNaN(id)) {
+            return res.status(400).json({error: 'To id must be a number'})
+        }
+
+        const toDo = toDos.find(toDo => toDo.id === id)
+
+        if (toDo) {
+            res.json(toDo)
+        } else {
+            res.status(404).json({
+                error: `TODO with id ${id} not found`
+            })
+        }
     }
 }
--- src/presentation/todos/routes.ts
+++ src/presentation/todos/routes.ts
@@ -5,9 +5,8 @@ export class ToDoRoutes {
     static get routes(): Router {
         const router = Router()
         const toDoController = new ToDosController()
-
         router.get('/', toDoController.getToDos)
-
+        router.get('/:id', toDoController.getToDoById)
         return router
     }
 }
