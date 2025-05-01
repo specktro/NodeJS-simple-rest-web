486f463d4523d297dd9d254574c245c525a6a1fa
@specktro <specktro@nonull.mx>
2025-05-01 14:56:44 -0600

    feat: Implement TodoRepository and TodoDatasource methods
    The `TodoRepositoryImpl` class has been introduced with full method implementations delegating to the datasource. Additionally, `TodoDatasourceImpl` methods are now fully implemented to interact with the database using Prisma. These changes enable CRUD operations for Todo entities.

--- src/implementation/datasource/todo.datasource.impl.ts
+++ src/implementation/datasource/todo.datasource.impl.ts
@@ -2,8 +2,12 @@ import {CreateToDoDTO, TodoDatasource, ToDoEntity, UpdateToDoDTO} from "../../do
 import {prisma} from "../../data/postgres";
 
 export class TodoDatasourceImpl implements TodoDatasource {
-    create(createToDoDTO: CreateToDoDTO): Promise<ToDoEntity> {
-        throw new Error("Method not implemented.");
+    async create(createToDoDTO: CreateToDoDTO): Promise<ToDoEntity> {
+        const toDo = await prisma.toDo.create({
+            data: createToDoDTO
+        })
+
+        return ToDoEntity.fromObject(toDo)
     }
 
     async getAll(): Promise<ToDoEntity[]> {
@@ -12,15 +16,34 @@ export class TodoDatasourceImpl implements TodoDatasource {
         return toDos.map(todo => ToDoEntity.fromObject(todo))
     }
 
-    findById(id: number): Promise<ToDoEntity> {
-        throw new Error("Method not implemented.");
+    async findById(id: number): Promise<ToDoEntity> {
+        const toDo = await prisma.toDo.findFirst({
+            where: {id: id}
+        })
+
+        if (!toDo) {
+            throw `ToDo with id ${id} not found`
+        }
+
+        return ToDoEntity.fromObject(toDo)
     }
 
-    updateById(updateToDoDTO: UpdateToDoDTO): Promise<ToDoEntity> {
-        throw new Error("Method not implemented.");
+    async updateById(updateToDoDTO: UpdateToDoDTO): Promise<ToDoEntity> {
+        const toDo = await this.findById(updateToDoDTO.id)
+        const updatedToDo = await prisma.toDo.update({
+            where: {id: updateToDoDTO.id},
+            data: updateToDoDTO.values,
+        })
+
+        return ToDoEntity.fromObject(updatedToDo)
     }
 
-    deleteById(id: number): Promise<ToDoEntity> {
-        throw new Error("Method not implemented.");
+    async deleteById(id: number): Promise<ToDoEntity> {
+        await this.findById(id)
+        const deletedToDo = await prisma.toDo.delete({
+            where: {id: id}
+        })
+
+        return ToDoEntity.fromObject(deletedToDo)
     }
 }
