4a546f9e309d8813658f1accbf62b6deb5f20cbb
@specktro <specktro@nonull.mx>
2025-05-24 14:52:45 -0600

    feat: Finish tests for app.ts

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -1,5 +1,18 @@
-describe('Testing app.ts', () => {
-    test('sample test', () => {
-        expect(true).toBeTruthy()
+import {Server} from "../src/presentation/server"
+import {envs} from "../src/config/envs";
+
+jest.mock("../src/presentation/server")
+
+describe('Should call server start with arguments', () => {
+    test('sample test', async () => {
+        await import("../src/app")
+        expect(Server).toHaveBeenCalledTimes(1)
+        expect(Server).toHaveBeenCalledWith({
+            port: envs.PORT,
+            publicPath: envs.PUBLIC_PATH,
+            routes: expect.any(Function)
+        })
+
+        expect(Server.prototype.start).toHaveBeenCalled()
     })
 })
